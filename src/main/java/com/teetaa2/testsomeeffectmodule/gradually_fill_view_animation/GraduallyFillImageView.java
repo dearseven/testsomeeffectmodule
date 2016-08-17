a862ee2036ad39a81b4be5b0e1757aa873a1a4dd
cyan <dearseven@sina.com>
2016-08-17 16:56:14 +0800

    m

--- src/main/java/com/teetaa2/testsomeeffectmodule/gradually_fill_view_animation/GraduallyFillImageView.java
+++ src/main/java/com/teetaa2/testsomeeffectmodule/gradually_fill_view_animation/GraduallyFillImageView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -29,17 +30,65 @@ public class GraduallyFillImageView extends ImageView {
     }
 
 
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        //super.onDraw(canvas);
+//
+//
+//        Drawable drawable = getDrawable();
+//        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+//        totalHeight = bitmap.getHeight();
+//        try {
+//            //裁剪
+//            Bitmap bitmap1 = bitmap.extractAlpha();
+//
+//            Log.i("percentHeight2",  getLeft()+", "+ getTop());
+//            //当bitmap.getHeight()==percentHeight的时候，则是全部绘制
+//            //Log.i("percentHeight2", "percentHeight:" + percentHeight);
+//            canvas.drawBitmap(bitmap, 0, 0, null);
+//            canvas.clipRect(0, bitmap.getHeight() - percentHeight, bitmap.getWidth(),
+//                    bitmap.getHeight());
+//            canvas.drawBitmap(bitmap1, 0, 0, paint1);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//
+//        }
+//    }
+
     @Override
     protected void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
         Drawable drawable = getDrawable();
         Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
         totalHeight = bitmap.getHeight();
-        super.onDraw(canvas);
         try {
             //裁剪
-            Bitmap bitmap1 = bitmap.extractAlpha();
             //当bitmap.getHeight()==percentHeight的时候，则是全部绘制
             //Log.i("percentHeight2", "percentHeight:" + percentHeight);
+
+            Log.i("m1",getWidth()+" "+getHeight());
+            Log.i("m2",bitmap.getWidth()+" "+bitmap.getHeight());
+
+            Matrix matrix = new Matrix();
+            float dx=1f,dy=1f;
+//            if(getWidth()>bitmap1.getWidth()&&getHeight()>bitmap1.getHeight()){
+//
+//            }else{
+//
+//            }
+            dx=(getWidth()*1.0f)/(bitmap.getWidth()*1.0f);
+            dy=(getWidth()*1.0f)/(bitmap.getWidth()*1.0f);
+            if(dx<dy){
+                dx=dy;
+            }else{
+                dy=dx;
+            }
+            matrix.postScale(dx,dy); //长和宽放大缩小的比例
+            bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+
+            Bitmap bitmap1 = bitmap.extractAlpha();
+            canvas.drawBitmap(bitmap, 0, 0, null);
             canvas.clipRect(0, bitmap.getHeight() - percentHeight, bitmap.getWidth(),
                     bitmap.getHeight());
             canvas.drawBitmap(bitmap1, 0, 0, paint1);
